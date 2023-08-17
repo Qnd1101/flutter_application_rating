226df33ee6e5449e5addd18ac96d53cd0ca8c06d
김민준 <107795830+Qnd1101@users.noreply.github.com>
2023-08-17 16:09:47 +0900

    Update meal_api.dart

--- lib/meal_api.dart
+++ lib/meal_api.dart
@@ -6,7 +6,7 @@ class MealApi {
   var url = "https://itmajor.cafe24.com/kiosk_api/rate.php";
   // 평가 저장
   Future<String> insert(String evalDate, double rating, String comment) async {
-    var header = {'Content-Typ': 'application/json'};
+    var header = {'Content-Type': 'application/json'};
     var body = jsonEncode({
       'mode': 'insert',
       'eval_date': evalDate,
