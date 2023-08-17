b7405aa74b251501a84cdd23cdb036acd5102fa0
김민준 <107795830+Qnd1101@users.noreply.github.com>
2023-08-17 16:08:39 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  TextEditingController controller = TextEditingController();
+  final TextEditingController _textEditingController = TextEditingController();
   var enabled = false;
   List<Score> score = [];
   double rate = 0;
@@ -23,6 +23,8 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     // TODO: implement initState
     super.initState();
+    score.add(Score(rate: 5, comment: '맛있어요'));
+    score.add(Score(rate: 1, comment: '맛없어요'));
   }
 
   @override
@@ -36,11 +38,12 @@ class _MyAppState extends State<MyApp> {
       ),
     );
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Column(
           children: [
             RatingBar.builder(
-              initialRating: 3,
+              initialRating: 0,
               minRating: 1,
               direction: Axis.horizontal,
               allowHalfRating: true,
@@ -50,47 +53,43 @@ class _MyAppState extends State<MyApp> {
                 Icons.star,
                 color: Colors.amber,
               ),
-              onRatingUpdate: (value) {
+              onRatingUpdate: (rating) {
+                print(rating);
                 setState(() {
+                  rate = rating;
                   enabled = true;
-                  rate = value;
                 });
               },
             ),
             TextFormField(
-              controller: controller,
+              controller: _textEditingController,
               enabled: enabled,
               decoration: const InputDecoration(
-                hintText: '한마디 부탁드려요 ㅜㅜ',
-                label: Text('한마디'),
-                border: OutlineInputBorder(),
-              ),
-              maxLength: 100,
+                  hintText: '맛을 평가해주세요.',
+                  label: Text('오늘의 급식 평가'),
+                  border: OutlineInputBorder()),
+              maxLength: 20,
             ),
             ElevatedButton(
-              onPressed: enabled
-                  ? () async {
-                      var api = MealApi();
-                      var evalDate = DateTime.now().toString().split(' ')[0];
-                      var res =
-                          await api.insert(evalDate, rate, controller.text);
-                      print(res);
-                      score.add(
-                        Score(
-                          rate: rate,
-                          comment: controller.text,
-                        ),
-                      );
-                      setState(() {
-                        listView;
-                      });
-                    }
-                  : null,
-              child: const Text('저장하기'),
-            ),
-            Expanded(
-              child: listView,
-            )
+                onPressed: enabled
+                    ? () async {
+                        var api = MealApi();
+                        var evalDate = DateTime.now().toString().split(' ')[0];
+                        var res = await api.insert(
+                            evalDate, rate, _textEditingController.text);
+                        print(res);
+
+                        score.add(
+                          Score(
+                              rate: rate, comment: _textEditingController.text),
+                        );
+                        setState(() {
+                          ListView;
+                        });
+                      }
+                    : null,
+                child: const Text('저장하기')),
+            Expanded(child: listView)
           ],
         ),
       ),
@@ -101,5 +100,6 @@ class _MyAppState extends State<MyApp> {
 class Score {
   double rate;
   String comment;
+
   Score({required this.rate, required this.comment});
 }
